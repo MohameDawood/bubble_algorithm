6595c4d7f6d0a6e0128e9fcec0e7915a880bc8bc
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-15 10:51:30 -0500

    code oganization

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,6 @@
 # In the name of Allah, the Entirely Merciful, the Especially Merciful
 def bubble_sort!(arr = [])
-
   (arr.length - 2).times do |i| # frist iteration
-
     # puts i #check
     is_sorted = true # frist I assumed that the array sorted but I check that
     length = arr.length - 1 - i # how many times in the second iteration
@@ -10,13 +8,12 @@ def bubble_sort!(arr = [])
       next unless arr[i] > arr[i + 1] # asking if the element is bigger than the next one
       arr[i], arr[i + 1] = arr[i + 1], arr[i] # swaping values
       is_sorted = false # rewrite the assumtion if it not sorted
+      
     end
+
     return arr if is_sorted # the assumtion was right so return arr
   end
   arr # as bang method return the same array with sorted values
 end
 
-
-
 p bubble_sort!(10, 5, 20, 8, 3, 9)
-
