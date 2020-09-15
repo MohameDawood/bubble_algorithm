7ea208d190ff6be44e27c3a53afdc7e1bafac559
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-15 10:35:35 -0500

    fixing linters errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,41 +7,16 @@ def bubble_sort!(arr = [])
   (arr.length - 2).times do |i| # frist iteration
 
     # puts i #check
-
-
-
     is_sorted = true # frist I assumed that the array sorted but I check that
-
-
-
     length = arr.length - 1 - i # how many times in the second iteration
-
-
-
     length.times do |i| # second iteration
-
       next unless arr[i] > arr[i + 1] # asking if the element is bigger than the next one
-
-
-
       arr[i], arr[i + 1] = arr[i + 1], arr[i] # swaping values
-
-
-
       is_sorted = false # rewrite the assumtion if it not sorted
-
     end
-
-
-
     return arr if is_sorted # the assumtion was right so return arr
-
   end
-
-
-
   arr # as bang method return the same array with sorted values
-
 end
 
 
