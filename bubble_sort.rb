8d58a7f628c4c7ad519bb2f8fcaa13923e30d140
Kevin Cotrina <kcotrinam92@gmail.com>
2020-09-15 11:02:28 -0500

    iteration fixed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,13 @@
 # In the name of Allah, the Entirely Merciful, the Especially Merciful
 def bubble_sort!(arr = [])
-  (arr.length - 2).times do |i| # frist iteration
+  (arr.length - 1).times do |i| # first iteration
     # puts i #check
     is_sorted = true # frist I assumed that the array sorted but I check that
     length = arr.length - 1 - i # how many times in the second iteration
-    length.times do |i| # second iteration
-      next unless arr[i] > arr[i + 1] # asking if the element is bigger than the next one
+    length.times do |j| # second iteration
+      next unless arr[j] > arr[j + 1] # asking if the element is bigger than the next one
       
-      arr[i], arr[i + 1] = arr[i + 1], arr[i] # swaping values
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] # swaping values
       is_sorted = false # rewrite the assumtion if it not sorted
     end
 
@@ -16,4 +16,4 @@ def bubble_sort!(arr = [])
   arr # as bang method return the same array with sorted values
 end
 
-p bubble_sort!(10, 5, 20, 8, 3, 9)
+p bubble_sort!([10, 5, 20, 1001, 8, 3, 1, 9])
