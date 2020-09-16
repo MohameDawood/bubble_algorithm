cbf22b724d6fd4e84130e95b1195e1a493454950
kevin Cotrina <50785733+kcotrinam92@users.noreply.github.com>
2020-09-16 14:15:45 -0500

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,4 +16,30 @@ def bubble_sort!(arr = [])
   arr # as bang method return the same array with sorted values
 end
 
-p bubble_sort!([10, 5, 20, 1001, 8, 3, 1, 9])
+def bubble_sort_by(array)
+  swap_made = true
+  total_iterations = 0
+  while swap_made
+    num_swaps = 0
+    total_elements = array.length - 1 - total_iterations
+    total_elements.times do |i|
+      result = yield(array[i], array[i + 1])
+      if result.positive?
+        temp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+        num_swaps += 1
+      end
+      next
+    end
+    swap_made = false unless num_swaps.positive?
+    total_iterations += 1
+  end
+  print array
+end
+
+# p bubble_sort!([10, 5, 20, 1001, 8, 3, 1, 9])
+
+# bubble_sort_by(["hi","hello","hey", "h"]) do |left,right|
+#     left.length - right.length
+# end
